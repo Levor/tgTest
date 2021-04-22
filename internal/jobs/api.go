63a0c481c358e9420effa4f146ae13283bb26404
vitaly <vetal.vasilevski@gmail.com>
2021-04-22 17:01:47 +0300

    fetch: Finished

--- internal/jobs/api.go
+++ internal/jobs/api.go
@@ -1,1 +1,36 @@
 package jobs
+
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"log"
+	"os"
+)
+
+func Start() {
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TG_TOKEN"))
+	if err != nil {
+		log.Panic(err)
+	}
+	bot.Debug = true
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+	updates, err := bot.GetUpdatesChan(u)
+	for update := range updates {
+		if update.Message == nil {
+			if update.ChannelPost != nil {
+				fileNames := GetFiles()
+				if update.ChannelPost.Text == "sent" || update.ChannelPost.Text == "Sent" {
+					for _, fileName := range fileNames {
+						msg := tgbotapi.NewDocumentUpload(update.ChannelPost.Chat.ID, "../bot_files/"+fileName)
+						bot.Send(msg)
+					}
+				}
+			}
+			continue
+		}
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Bot work at the channel"+os.Getenv("CHANNEL_NAME"))
+		bot.Send(msg)
+	}
+}
--- internal/jobs/files.go
+++ internal/jobs/files.go
@@ -1,1 +1,21 @@
 package jobs
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+func GetFiles() []string {
+	files, err := ioutil.ReadDir(os.Getenv("FILES_PATH"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var filesNames []string
+	for _, file := range files {
+		filesNames = append(filesNames, file.Name())
+
+	}
+	return filesNames
+}
